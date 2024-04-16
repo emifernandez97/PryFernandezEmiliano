2377f0b0537d42c92a87c92811b5008192aa391f
Emifernandez97 <emifernandez_97@hotmail.com>
2024-04-16 13:40:35 -0300

    Conexion a la Base de datos

---
+++ ClsConexionBDD.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Mysql.Data.MySqlClient;
+using System.Data.OleDb;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+using System.IO;
+
+
+
+namespace PryFernandezE
+{
+    internal class ClsConexionBDD
+    {
+        public void ConexionBaseDatos(ToolStripStatusLabel TstpBD) 
+        {
+            try 
+            {
+                string Conexion =
+                "Server=127.0.0.1" +
+                ";Database=juegorol" +
+                ";Uid=root" +
+                ";Pwd=;";
+
+                MySqlConnection Conn = new MySqlConnection(Conexion);
+                Conn.Open();
+
+                /*
+                TstpBD.Text = "Conectado";
+                TstpBD.BackColor = Color.Green;
+                  */
+            } 
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+    }
+}
